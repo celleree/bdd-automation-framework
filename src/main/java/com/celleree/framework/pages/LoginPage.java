58da91cde4aae1097bc86da4672e7073f1bfad98
celleree <113010938+celleree@users.noreply.github.com>
2026-08-20 13:17:52 -0700

    Add SauceDemo login page object

---
+++ src/main/java/com/celleree/framework/pages/LoginPage.java
@@ -0,0 +1,42 @@
+package com.celleree.framework.pages;
+
+import com.celleree.framework.utils.BrowserManager;
+import com.celleree.framework.utils.ConfigReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Page object for the SauceDemo login page.
+ */
+public class LoginPage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    private final By usernameInput = By.id("user-name");
+    private final By passwordInput = By.id("password");
+    private final By loginButton = By.id("login-button");
+    private final By productsTitle = By.className("title");
+
+    public LoginPage() {
+        this.driver = BrowserManager.getDriver();
+        this.wait = BrowserManager.getWait();
+    }
+
+    public void open() {
+        driver.get(ConfigReader.get("ui.base.url"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInput));
+    }
+
+    public void login(String username, String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInput)).sendKeys(username);
+        driver.findElement(passwordInput).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+
+    public String getProductsTitle() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(productsTitle)).getText();
+    }
+}
