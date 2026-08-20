b50c1c6f5f3dbc43043c208ae8662c1a31b2dc36
celleree <113010938+celleree@users.noreply.github.com>
2026-08-20 13:18:11 -0700

    Add Cucumber TestNG runner

---
+++ src/test/java/com/celleree/framework/runners/TestRunner.java
@@ -0,0 +1,14 @@
+package com.celleree.framework.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com.celleree.framework.steps",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html"
+        })
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
